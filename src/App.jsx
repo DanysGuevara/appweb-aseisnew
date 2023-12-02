116e5d1d7a0c45c8f5f3c0014d8cd133e336507c
DanysGuevara <ga19014@ues.edu.sv>
2023-12-02 00:29:32 -0600

    Add ImagePost component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavbarBeforeLogin from './components/aseis-navbar-before.jsx';
+import ImagePost from './components/aseis-image.jsx';
 function App() {
   
   return (
     <>
       <NavbarBeforeLogin />
+      <ImagePost />
     </>
   );
 }
